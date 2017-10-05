73e707f3d7d65c729ba17fd098214cfc56129ea1
Linux O'Beardly <linux.obeardly@gmail.com>
2017-10-05 01:08:29 -0400

    First upload

---
+++ cels_conv.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+#Hello World End of Chapter 3 Exercise 2
+#Convert Celsius to Farhrenheit
+F = raw_input("Please enter temperature in degrees Farhenheit: ")
+C = 5.0 / 9 * (int (F) - 32 )
+print "Your current temperature is ",C," degrees Celsius. Have a nice day!"
---
+++ pers_info.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+#Hello Word End of Chapter 1 Exercise 2
+print "My name is Mark R. White" 
+print "I was born 03 July 1978" 
+print "My favorite color is purple."

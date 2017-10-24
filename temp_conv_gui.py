e1fab3b7755b41e7660df15e029b0cdc57ffa66a
Linux O'Beardly <linux.obeardly@gmail.com>
2017-10-24 01:56:08 -0400

    Delete temp_conv_gui.py

--- temp_conv_gui.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-#Hello World End of Chapter 6 Exercise 1
-#Convert Celsius to Farhrenheit
-import easygui
-F = easygui.integerbox("Please enter temperature in degrees Farhenheit: ")
-C = 5.0 / 9 * ( F - 32 )
-easygui.msgbox("Your current temperature is " + str(C))

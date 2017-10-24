6fabc9eda80098ab0deae862e8ab6d4e7251dedd
obeardly <linux.obeardly@gmail.com>
2017-10-24 01:45:17 -0400

    24OCT2017_0145

---
+++ chap2/min_hour_day.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+daysweek = int(7)
+hoursday = int(24)
+minhour = int(60)
+minweek = minhour * hoursday * daysweek
+print "There are",minweek,"minutes in a week."
---
+++ chap2/min_hour_day2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+daysweek = int(7)
+hoursday = int(26)
+minhour = int(60)
+minweek = minhour * hoursday * daysweek
+print "If there were 26 hours in a day, there would be",minweek,"minutes in a week."

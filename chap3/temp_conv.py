fa0af8fd5c3111af21229438db081a70929dcaeb
Linux O'Beardly <linux.obeardly@gmail.com>
2017-10-24 17:55:16 -0400

    24OCT2017_1755

---
+++ chap3/temp_conv.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+#Hello World End of Chapter 3 Exercise 2
+#Convert Celsius to Farhrenheit
+F = float(raw_input("Please enter temperature in degrees Farhenheit: "))
+C = 5.0 / 9 * ( F - 32 )
+print "Your current temperature is ",C," degrees Celsius. Have a nice day!"

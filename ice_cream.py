c029ad34ba48488ff71c64e049fb67aa6258f3c5
Linux O'Beardly <linux.obeardly@gmail.com>
2017-10-24 01:52:50 -0400

    Delete ice_cream.py

--- ice_cream.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-#Hello World Listing 6.1
-import easygui
-flavor = easygui.buttonbox("What is your favorite flavor of ice cream?",
-        choices = [ 'Vanilla', 'Chocolate', 'Strawberry', 'Neopolitan' ] )
-easygui.msgbox("You picked: " + flavor)

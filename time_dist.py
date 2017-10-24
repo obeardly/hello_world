4844df2e672f8e91ce4bbf0975bb9dc8fe1d64f6
Linux O'Beardly <linux.obeardly@gmail.com>
2017-10-24 01:56:30 -0400

    Delete time_dist.py

--- time_dist.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-# Hello World End of Chapter 3 Exercise 3
-dist = raw_input("In miles, please tell me how far you are going today: ")
-speed = raw_input("In MPH, please tell me how fast you will be driving: ")
-time = float(dist) / float(speed)
-print "Your travel time today will be approximately ",time," hours."

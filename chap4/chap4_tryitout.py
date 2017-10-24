5ebbb9e466c1b65193c7ec9575dca66e1786a5fd
obeardly <linux.obeardly@gmail.com>
2017-10-24 02:04:28 -0400

    24OCT2017_0204

---
+++ chap4/chap4_tryitout.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+one = float(raw_input("\nPlease input a number with a decimal: "))
+print "\nYour number as a float: ",one,"\n"
+print "Your number an an integer: ",int(one),"\n"
